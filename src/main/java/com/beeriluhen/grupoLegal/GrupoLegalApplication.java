7a914288b90fa20b7abd61378196990cb8e2b4e3
EricoPanassol <ericopanassol@gmail.com>
2023-04-03 17:50:19 -0300

    First spring boot files

---
+++ src/main/java/com/beeriluhen/grupoLegal/GrupoLegalApplication.java
@@ -0,0 +1,13 @@
+package com.beeriluhen.grupoLegal;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GrupoLegalApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GrupoLegalApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/beeriluhen/grupoLegal/GrupoLegalApplicationTests.java
@@ -0,0 +1,13 @@
+package com.beeriluhen.grupoLegal;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GrupoLegalApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
